4e463685b362e9cf3c7176b8814665acd08c09b2
FlowerF <1647231392@qq.com>
2022-06-13 22:17:17 +0800

    feat: 完成LoginForm组件

--- src/components/LoginForm/index.tsx
+++ src/components/LoginForm/index.tsx
@@ -1,26 +1,38 @@
-import { Button, Form, Toast } from '@douyinfe/semi-ui';
+import { Button, Form } from '@douyinfe/semi-ui';
 import styles from './style.module.scss';
 
 const LoginForm = () => {
   const handleSubmit = (values: Record<string, any>) => {
     console.log(values);
-    Toast.info('表单已提交');
+    // Toast.info('表单已提交');
   };
 
   return (
-    <div className={styles['login-form']}>
-      <Form onSubmit={(values) => handleSubmit(values)} style={{ width: 400 }}>
-        {({ values }) => (
-          <>
-            <Form.Input field="phone" label="PhoneNumber" style={{ width: '100%' }} placeholder="Enter your phone number" />
-            <Form.Input field="password" label="Password" style={{ width: '100%' }} placeholder="Enter your password" />
-            <Form.Checkbox field="agree" noLabel>I have read and agree to the terms of service</Form.Checkbox>
-            <div>
-              <Button disabled={!values.agree} htmlType="submit" type="tertiary">Log in</Button>
-            </div>
-          </>
-        )}
-      </Form>
+    <div className={styles['login-container']}>
+      <img className={styles.background} src="/milk-banner.png" alt="背景图片" />
+      <div className={styles['login-form']}>
+        <h3 className={styles.title}>特仑苏牛奶溯源系统</h3>
+        <Form onSubmit={(values) => handleSubmit(values)} style={{ width: 400 }}>
+          <Form.Input
+            field="phone"
+            label="用户名"
+            style={{ width: '100%' }}
+            placeholder="请输入您的用户名"
+            rules={[{ required: true, message: '用户名未填写' }]}
+          />
+          <Form.Input
+            field="password"
+            label="密码"
+            type="password"
+            style={{ width: '100%' }}
+            placeholder="请输入您的密码"
+            rules={[{ required: true, message: '密码未填写' }]}
+          />
+          <div className={styles['login-button']}>
+            <Button htmlType="submit" type="tertiary">登录</Button>
+          </div>
+        </Form>
+      </div>
     </div>
   );
 };

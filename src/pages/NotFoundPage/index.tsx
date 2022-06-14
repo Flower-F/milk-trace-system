45c0bea060d42dbbdb7c6c75bbfd5a368c0bcbcc
FlowerF <1647231392@qq.com>
2022-06-14 20:39:07 +0800

    feat: 添加404页面

--- src/pages/NotFoundPage/index.tsx
+++ src/pages/NotFoundPage/index.tsx
@@ -1,5 +1,15 @@
+import { Empty } from '@douyinfe/semi-ui';
+import { IllustrationConstruction, IllustrationConstructionDark } from '@douyinfe/semi-illustrations';
+import styles from './style.module.scss';
+
 const NotFoundPage = () => (
-  <div>404 Not Found</div>
+  <Empty
+    image={<IllustrationConstruction className={styles['not-icon']} />}
+    darkModeImage={<IllustrationConstructionDark className={styles['not-icon']} />}
+    title="404 Not Found"
+    className={styles['not-container']}
+    description="没有这个界面哦"
+  />
 );
 
 export default NotFoundPage;

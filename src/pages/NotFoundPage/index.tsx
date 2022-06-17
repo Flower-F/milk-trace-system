e52b45960caf3da64746ad17ec504adbc910b205
FlowerF <1647231392@qq.com>
2022-06-17 23:47:15 +0800

    feat: 为404页面添加返回首页按钮

--- src/pages/NotFoundPage/index.tsx
+++ src/pages/NotFoundPage/index.tsx
@@ -1,15 +1,28 @@
-import { Empty } from '@douyinfe/semi-ui';
+import { Button, Empty } from '@douyinfe/semi-ui';
 import { IllustrationConstruction, IllustrationConstructionDark } from '@douyinfe/semi-illustrations';
+import { useNavigate } from 'react-router-dom';
 import styles from './style.module.scss';
 
-const NotFoundPage = () => (
-  <Empty
-    image={<IllustrationConstruction className={styles['not-icon']} />}
-    darkModeImage={<IllustrationConstructionDark className={styles['not-icon']} />}
-    title="404 Not Found"
-    className={styles['not-container']}
-    description="没有这个界面哦"
-  />
-);
+const NotFoundPage = () => {
+  const navigate = useNavigate();
+
+  const navigateToHome = () => {
+    navigate('/admin');
+  };
+
+  return (
+    <div className={styles['not-container']}>
+      <Empty
+        image={<IllustrationConstruction className={styles['not-icon']} />}
+        darkModeImage={<IllustrationConstructionDark className={styles['not-icon']} />}
+        title="404 Not Found"
+        description="没有这个界面哦"
+      />
+      <div className={styles.button}>
+        <Button type="primary" size="large" onClick={navigateToHome}>返回首页</Button>
+      </div>
+    </div>
+  );
+};
 
 export default NotFoundPage;
